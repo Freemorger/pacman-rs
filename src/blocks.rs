edebef3497b53b90119c060584ee425ca1720a2b
freemorger <freemorger@gmail.com>
2024-11-01 02:33:31 +0300

    v0.2.1

--- src/blocks.rs
+++ src/blocks.rs
@@ -1,7 +1,34 @@
-mod widgets {
-    pub struct Block {
-        imgsrc: String,
-        blocked: bool,
-        has_point: bool,
+pub mod widgets {
+    use ggez::mint::Point2;
+    use std::path::PathBuf;
+
+    pub struct Player {
+        texture_path: PathBuf,
+        pos: Point2<f32>,
+        points: i32,
+    }
+
+    impl Player {
+        pub fn new(texture: PathBuf, position: Point2<f32>, points: i32) -> Player {
+            let player = Player {
+                texture_path: texture,
+                pos: position,
+                points: points,
+            };
+            player
+        }
+        pub fn default(texture: PathBuf, gtx: ggez::graphics::GraphicsContext) -> Player {
+            let (sizeX, sizeY) = gtx.size();
+            let playerpos: Point2<f32> = Point2 {
+                x: (sizeX / 2.0),
+                y: (sizeY / 2.0),
+            };
+            let player = Player {
+                texture_path: texture,
+                pos: playerpos,
+                points: 0,
+            };
+            player
+        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
+use blocks::widgets::Player;
 use ggez::event::{self, EventHandler, EventLoop};
 use ggez::filesystem::resources_dir;
-use ggez::graphics::{self, Color, DrawParam};
+use ggez::graphics::{self, set_window_title, Color, DrawParam};
 use ggez::graphics::{GraphicsContext, Image};
 use ggez::mint::Point2;
 use ggez::{Context, ContextBuilder, GameResult};
@@ -9,6 +10,9 @@ use std::path::PathBuf;
 use std::{env, fs};
 mod blocks;
 
+const VERSION: &str = "v0.2";
+const AUTHOR: &str = "freemorger";
+
 fn main() {
     let curdir = env::current_dir().unwrap();
     let (mut ctx, event_loop) = ContextBuilder::new("pacman-rs", "freemorger")
@@ -16,30 +20,52 @@ fn main() {
         .build()
         .expect("cant create context");
 
+    set_window_title(&ctx, &format!("Pacman-rs, {} by {}", VERSION, AUTHOR));
     let game = Game::new(&mut ctx);
 
     event::run(ctx, event_loop, game);
 }
 
 struct Game {
-    imgpaths: Vec<String>,
+    imgpaths: Vec<PathBuf>,
+    player: blocks::widgets::Player,
     //game_map: Vec<Vec<u16>>
 }
 
 impl Game {
     pub fn new(_ctx: &mut Context) -> Game {
-        let mut imgs: Vec<String> = vec![];
+        let player_start_texture: &str = "pacman.png";
+
+        let mut imgs: Vec<PathBuf> = vec![];
         let img_dir = PathBuf::from("img");
         let files = fs::read_dir(img_dir).unwrap();
+        let curdir = env::current_dir().unwrap();
 
         for file in files {
             match file {
-                Ok(f) => imgs.push(f.path().to_str().unwrap().to_string()),
+                Ok(f) => imgs.push(curdir.join(f.path().to_str().unwrap())),
                 Err(e) => eprintln!("Error reading images: {:?}", e),
             }
         }
 
-        Game { imgpaths: imgs }
+        let player_texture = imgs
+            .iter()
+            .find(|s| {
+                s.as_os_str()
+                    .to_str()
+                    .unwrap()
+                    .contains(player_start_texture)
+            })
+            .unwrap();
+        let playerPos: Point2<f32> = Point2 { x: 0.0, y: 0.0 };
+        let mut player = blocks::widgets::Player::new(player_texture.clone(), playerPos, 0);
+
+        //println!("{:#?}", player_texture);
+
+        Game {
+            imgpaths: imgs,
+            player: player,
+        }
     }
 }
 
@@ -59,7 +85,7 @@ impl EventHandler for Game {
         let test_dest: Point2<f32> = Point2 {
             x: (55.0),
             y: (44.0),
-        };
+        }; // some cords
         canvas.draw(&testimage, DrawParam::new().dest(test_dest));
 
         canvas.finish(ctx)
